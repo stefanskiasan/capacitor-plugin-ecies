ee7b605e858b2bc50045b318ac008e4d52e52c0e
asanstefanski <asan.stefanski@advisori.de>
2023-03-22 23:28:34 +0100

    Update to Capacitor 4

--- ios/Plugin/EciesPlugin.swift
+++ ios/Plugin/EciesPlugin.swift
@@ -9,49 +9,49 @@ import CryptorECC
 @objc(EciesPlugin)
 public class EciesPlugin: CAPPlugin {
 
-    @objc func generateKeys(_ call: CAPPluginCall) {
-            do {
-             let p256PrivateKey = try ECPrivateKey.make(for: .prime256v1)
-             let eccPublicKey = try p256PrivateKey.extractPublicKey()
+     @objc func generateKeys(_ call: CAPPluginCall) {
+                do {
+                 let p256PrivateKey = try ECPrivateKey.make(for: .prime256v1)
+                 let eccPublicKey = try p256PrivateKey.extractPublicKey()
+                    call.resolve([
+                        "publicKey": eccPublicKey.pemString,
+                        "privateKey": p256PrivateKey.pemString
+                      ])
+                } catch {
+                  print("\(error)")
+                }
+     }
+     @objc func encrypt(_ call: CAPPluginCall) {
+                let pemPublic163k1 = call.getString("publickey") ?? ""
+                let plaintext      = call.getString("plaintext") ?? ""
+                var encryptedData  = ""
+
+                do {
+                     encryptedData = try plaintext.encrypt(with: ECPublicKey(der: Data(base64Encoded: pemPublic163k1)!)).base64EncodedString();
+                } catch {
+                  print("\(error)")
+                }
+
                 call.resolve([
-                    "publicKey": eccPublicKey.pemString,
-                    "privateKey": p256PrivateKey.pemString
-                  ])
-            } catch {
-              print("\(error)")
-            }
-    }
-    @objc func encrypt(_ call: CAPPluginCall) {
-            let pemPublic163k1 = call.getString("publickey") ?? ""
-            let plaintext      = call.getString("plaintext") ?? ""
-            var encryptedData  = ""
+                    "value": encryptedData
+                ])
+     }
+     @objc func decrypt(_ call: CAPPluginCall) {
 
-            do {
-                 encryptedData = try plaintext.encrypt(with: ECPublicKey(der: Data(base64Encoded: pemPublic163k1)!)).base64EncodedString();
-            } catch {
-              print("\(error)")
-            }
+                let pemPrivate163k1 = call.getString("privatekey") ?? ""
+                let crypttext       = call.getString("crypttext") ?? ""
+                var decryptedData:String?   = ""
 
-            call.resolve([
-                "value": encryptedData
-            ])
-    }
-    @objc func decrypt(_ call: CAPPluginCall) {
+                do {
+                    var key = try ECPrivateKey(key: pemPrivate163k1);
+                    var tempdecryptedData = try Data(base64Encoded: crypttext)!.decrypt(with: key )
+                    decryptedData = String(data: tempdecryptedData, encoding: .utf8)
+                } catch {
+                  print("\(error)")
+                }
 
-            let pemPrivate163k1 = call.getString("privatekey") ?? ""
-            let crypttext       = call.getString("crypttext") ?? ""
-            var decryptedData:String?   = ""
-
-            do {
-                var key = try ECPrivateKey(key: pemPrivate163k1);
-                var tempdecryptedData = try Data(base64Encoded: crypttext)!.decrypt(with: key )
-                decryptedData = String(data: tempdecryptedData, encoding: .utf8)
-            } catch {
-              print("\(error)")
-            }
-
-            call.resolve([
-                "value": decryptedData
-            ])
-    }
+                call.resolve([
+                    "value": decryptedData
+                ])
+     }
 }
